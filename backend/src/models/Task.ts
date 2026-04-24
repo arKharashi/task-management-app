6c5192d8281ec6ee302189c27799c579fc4b8ab2
Abdulrahman Alkharashi <ar_kharashi@Abdulrahmans-MacBook-Pro.local>
2026-04-24 20:36:04 +0300

    feat: create task model

---
+++ backend/src/models/Task.ts
@@ -0,0 +1,32 @@
+import mongoose from "mongoose";
+
+const taskSchema = new mongoose.Schema(
+  {
+    title: {
+      type: String,
+      required: true,
+    },
+    description: {
+      type: String,
+    },
+    completed: {
+      type: Boolean,
+      default: false,
+    },
+    priority: {
+      type: String,
+      enum: ["low", "medium", "high"],
+      default: "medium",
+    },
+    user: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+  },
+  { timestamps: true }
+);
+
+const Task = mongoose.model("Task", taskSchema);
+
+export default Task;
