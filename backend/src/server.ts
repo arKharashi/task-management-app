fa204de82cb0288b533cbb690c9f69d05f7b9c39
Abdulrahman Alkharashi <ar_kharashi@Abdulrahmans-MacBook-Pro.local>
2026-04-24 19:22:36 +0300

    feat: authentication Apis (register & login)

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -2,15 +2,17 @@ import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
 import connectDB from "./config/db";
+import authRoutes from "./routes/authRoutes";
 
 dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 5001;
-const mongoUri = process.env.MONGODB_URI;
 
 app.use(cors());
 app.use(express.json());
+
+app.use("/api/auth", authRoutes);
 
 app.get("/health", (_req, res) => {
   res.status(200).json({ message: "Backend is running" });
